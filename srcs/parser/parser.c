554a1d4b9972186736cdf61b2651be566ce1d03c
David Agredano <davidagredano@gmail.com>
2025-05-23 16:49:23 +0200

    feat: Add redirect creation to the parser

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,18 +6,50 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 20:01:35 by dagredan          #+#    #+#             */
-/*   Updated: 2025/05/22 20:43:51 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/05/23 11:53:16 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	handle_new_process(t_parser *parser)
+static int	add_new_process(t_parser *parser)
 {
 	parser->process = create_process();
 	if (!parser->process)
 		return (0);
 	append_process(parser->process, &parser->processes);
+	return (1);
+}
+
+static int	add_new_redirect(t_parser *parser)
+{
+	t_redirect	*new_redirect;
+
+	new_redirect = create_redirect(parser->token, parser->token->next);
+	if (!new_redirect)
+		return (0);
+	append_redirect(new_redirect, &parser->process->redirects);
+	return (1);
+}
+
+static int	handle_tokens(t_parser *parser)
+{
+	if (is_redirection(parser->token))
+	{
+		if (!add_new_redirect(parser))
+			return (0);
+		parser->token = parser->token->next->next;
+	}
+	else if (is_word(parser->token))
+	{
+		parser->token = parser->token->next; // TODO
+	}
+	else if (is_pipe(parser->token))
+	{
+		if (!add_new_process(parser))
+			return (0);
+		parser->token = parser->token->next;
+	}
 	return (1);
 }
 
@@ -29,19 +61,14 @@ t_process	*parse_tokens(t_token *tokens)
 		return (NULL);
 	parser.token = tokens;
 	parser.processes = NULL;
-	if (!handle_new_process(&parser))
+	if (!add_new_process(&parser))
 		return (NULL);
 	while (parser.token)
 	{
-		if (is_redirection(parser.token))
-			parser.token = parser.token->next; // TODO
-		else if (is_word(parser.token))
-			parser.token = parser.token->next; // TODO
-		else if (is_pipe(parser.token))
+		if (!handle_tokens(&parser))
 		{
-			if (!handle_new_process(&parser))
-				return (free_processes(&parser.processes), NULL);
-			parser.token = parser.token->next;
+			free_processes(&parser.processes);
+			return (NULL);
 		}
 	}
 	return (parser.processes);
