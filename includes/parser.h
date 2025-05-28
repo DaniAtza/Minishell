92896bbb8ce19fe7923c4870944d37205cb52aca
David Agredano <davidagredano@gmail.com>
2025-05-28 22:06:13 +0200

    feat: Add command search using PATH (#11)
    * feat: Add command search using PATH
    
    * test: Add tests for get_pathname
    
    Bona feina, el codi s'enten super be.

--- includes/parser.h
+++ includes/parser.h
@@ -6,14 +6,14 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:26:24 by dagredan          #+#    #+#             */
-/*   Updated: 2025/05/24 19:41:41 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:41:36 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-#include "types.h"
+# include "types.h"
 
 typedef struct s_parser
 {
@@ -41,11 +41,11 @@ void		free_args(char ***args_addr);
 t_process	*parse_tokens(t_token *tokens);
 
 /* validation.c */
-int 		validate_syntax(t_data *data);
+int			validate_syntax(t_data *data);
 
 /* utils.c */
-int 		is_pipe(t_token *token);
-int 		is_redirection(t_token *token);
-int 		is_word(t_token *token);
+int			is_pipe(t_token *token);
+int			is_redirection(t_token *token);
+int			is_word(t_token *token);
 
 #endif
