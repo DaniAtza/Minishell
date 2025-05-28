9376288a76121bb5c3f862b4e7b14872e9bf73d6
David Agredano <davidagredano@gmail.com>
2025-05-28 22:06:13 +0200

    feat: Add command search using PATH (#11)
    * feat: Add command search using PATH
    
    * test: Add tests for get_pathname
    
    Bona feina, el codi s'enten super be.

---
+++ includes/execute.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: datienza <datienza@student.42barcelo>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/24 17:59:29 by datienza          #+#    #+#             */
+/*   Updated: 2025/05/28 15:41:34 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTE_H
+# define EXECUTE_H
+
+# define DEF_PATH "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+
+char	*get_pathname(char *cmd_name);
+
+#endif
