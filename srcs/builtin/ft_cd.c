32b850840a0233396705893c4f39aecc3b52d0db
Dani-42 <d.atienza89@gmail.com>
2025-06-17 22:50:24 +0200

    ft_cd-check_arg

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -1,32 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exe_builtin.c                                      :+:      :+:    :+:   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: datienza <datienza@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/07 12:01:22 by datienza          #+#    #+#             */
-/*   Updated: 2025/06/07 12:06:15 by datienza         ###   ########.fr       */
+/*   Created: 2025/06/17 18:32:31 by datienza          #+#    #+#             */
+/*   Updated: 2025/06/17 18:33:06 by datienza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	exe_builtin(char **cmd, t_env *env_list)
+int	check_arg_error(char **arg)
 {
-	if (!cmd)
+	size_t	i;
+
+	i = 0;
+	while (arg[i])
+		i++;
+	if ((i - 1) == 0)
+		return (0);
+	else if ((i - 1) >= 2)
+		return(ft_putendl_fd("cd: too many arguments", STDERR_FILENO), 1);
+	return (0);
+}
+
+
+int	ft_cd(char **arg)
+{
+
+	if (check_arg_error(arg))
 		return (1);
-	if (ft_strcmp(cmd[0], "echo") == 0)
-		ft_echo(cmd);
-	else if (ft_strcmp(cmd[0], "pwd") == 0)
-		ft_pwd();
-	else if (ft_strcmp(cmd[0], "cd") == 0)
-		ft_cd(cmd);
-	else if (ft_strcmp(cmd[0], "env") == 0)
-		print_env_list(env_list);
-	else if (ft_strcmp(cmd[0], "export") == 0)
-		ft_export(cmd, &env_list);
-	else if (ft_strcmp(cmd[0], "unset") == 0)
-		ft_unset(cmd, &env_list);
+	
+	if (!arg[1] || !(ft_strcmp(arg[1], "~")))
+	{
+		if (chdir("/home") == -1)
+			return (1);
+	}
 	return (0);
 }
