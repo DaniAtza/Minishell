7cb08898951d5f904710a08684d3ef5611e8e1ba
Daniel Atienza Romero <datienza@cbr13s5.42barcelona.com>
2025-06-22 12:32:56 +0200

    cd ~ -

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: datienza <datienza@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 18:32:31 by datienza          #+#    #+#             */
-/*   Updated: 2025/06/17 18:33:06 by datienza         ###   ########.fr       */
+/*   Updated: 2025/06/22 12:06:41 by datienza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,27 @@ int	check_arg_error(char **arg)
 	if ((i - 1) == 0)
 		return (0);
 	else if ((i - 1) >= 2)
-		return(ft_putendl_fd("cd: too many arguments", STDERR_FILENO), 1);
+		return (ft_putendl_fd("cd: too many arguments", STDERR_FILENO), 1);
 	return (0);
 }
 
 int	update_working_directory(char *directory, t_env **env_list)
 {
 	char	*name_value;
-	
+
 	if (chdir(directory) == -1)
 		return (perror(directory), -1);
 	if (search_env("PWD", *env_list))
 	{
 		name_value = ft_strjoin("OLDPWD=", search_env("PWD", *env_list));
-			if (!name_value)
-				print_and_return_error("cd malloc", 1);
+		if (!name_value)
+			print_and_return_error("cd malloc", 1);
 		update_env_node(name_value, env_list);
 		free (name_value);
 	}
 	name_value = ft_strjoin("PWD=", getcwd(NULL, 0));
-		if (!name_value)
-			print_and_return_error("cd malloc", 1);
+	if (!name_value)
+		print_and_return_error("cd malloc", 1);
 	update_env_node(name_value, env_list);
 	free (name_value);
 	return (0);
@@ -64,15 +64,21 @@ int	update_working_directory(char *directory, t_env **env_list)
 
 int	ft_cd(char **arg, t_env **env_list)
 {
-
 	if (check_arg_error(arg))
 		return (1);
-	if (!arg[1])
+	if (!arg[1] || !(ft_strcmp(arg[1], "~")))
 	{
 		if (!search_env("HOME", *env_list))
-			return(ft_putendl_fd("cd: HOME not set", STDERR_FILENO), 1);
+			return (ft_putendl_fd("cd: HOME not set", STDERR_FILENO), 1);
 		if (update_working_directory(search_env("HOME", *env_list), env_list))
-			return (1); 
+			return (1);
+	}
+	else if (!(ft_strcmp(arg[1], "-")))
+	{
+		if (!search_env("OLDPWD", *env_list))
+			return (ft_putendl_fd("cd: OLDPWD not set", STDERR_FILENO), 1);
+		if (update_working_directory(search_env("OLDPWD", *env_list), env_list))
+			return (1);
 	}
 	else
 	{
