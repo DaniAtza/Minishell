d3a3f24b32a60b1a585049dd9042d987f5f6f5aa
David Agredano <davidagredano@gmail.com>
2025-07-13 16:49:30 +0200

    fix: init_pipeline (#35)
    * fix: init_pipeline return values
    
    * fix: update error handling in init_pipeline
    
    * fix: History handling for space-only commands
    
    ---------
    
    Co-authored-by: David Agredano Granell <dagredan@student.42barcelona.com>

--- srcs/data/data.c
+++ srcs/data/data.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 21:32:30 by dagredan          #+#    #+#             */
-/*   Updated: 2025/07/08 19:06:17 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:40:54 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,18 @@ void	free_data(t_data *data)
 
 int	init_pipeline(t_pipeline *pipeline)
 {
-	// TODO: Trim string (an all spaces string fail)
-	if (ft_strlen(pipeline->line) == 0)
-		return (-1);
 	pipeline->tokens = tokenize_line(pipeline->line);
 	if (!pipeline->tokens)
-		print_and_return_error("Error: tokenize_tokens", -1);
+		return (-1);
 	if (validate_syntax(pipeline) != 0)
-		print_and_return_error("Error: validate_syntax: Syntax error", -1);
+		return (print_and_return_error("Error: Syntax error", -1));
 	pipeline->processes = parse_tokens(pipeline->tokens);
 	if (!pipeline->processes)
-		print_and_return_error("Error: parse_tokens", -1);
+		return (print_and_return_error("Error: parse_tokens", -1));
 	pipeline->process_count = count_processes(pipeline->processes);
 	pipeline->pipes = create_pipes(pipeline->process_count - 1);
 	if (!pipeline->pipes)
-		print_and_return_error("Error: parse_tokens", -1);
+		return (print_and_return_error("Error: create_pipes", -1));
 	assign_pipes_to_processes(pipeline->pipes, pipeline);
 	return (0);
 }
