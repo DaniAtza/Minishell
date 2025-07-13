ffe7cd48881c311cd47d7ae61786aea668802fb7
David Agredano <davidagredano@gmail.com>
2025-07-13 21:54:48 +0200

    fix: update exit status handling (#38)
    Co-authored-by: David Agredano Granell <dagredan@student.42barcelona.com>

--- includes/signals.h
+++ includes/signals.h
@@ -6,7 +6,7 @@
 /*   By: datienza <datienza@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 22:34:39 by datienza          #+#    #+#             */
-/*   Updated: 2025/07/13 14:19:21 by datienza         ###   ########.fr       */
+/*   Updated: 2025/07/13 20:25:01 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ typedef struct s_signal_backup
 void				setup_signals(void);
 void				signal_handler(int signum);
 void				setup_child_signals(void);
-void				handle_child(int signum);
 void				restore_signals(t_signal_backup backup);
 t_signal_backup		set_execution_signals(void);
 
