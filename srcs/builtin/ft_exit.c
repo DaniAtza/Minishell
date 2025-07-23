a5c66f661f3a5462e9f3298e8e849f13afcb5f61
Dani-42 <d.atienza89@gmail.com>
2025-07-23 08:59:25 +0200

    no-num-arg

--- srcs/builtin/ft_exit.c
+++ srcs/builtin/ft_exit.c
@@ -12,8 +12,39 @@
 
 #include "minishell.h"
 
+static int	no_num(char *arg)
+{
+	int	i;
+
+	i = 0;
+	if(!arg)
+		return(1);
+	while(arg[i])
+	{
+		if(!ft_isdigit(arg[i]))
+		{
+			ft_putendl_fd("exit: ", STDERR_FILENO);
+			ft_putendl_fd(arg, STDERR_FILENO);
+			ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
 void	ft_exit(char **argv, t_data *data)
 {
-    if (!argv[1])
-        exit(data->last_exit_status);
+
+	if(!no_num(argv[1]))
+	{
+		data->last_exit_status = 2;
+		return ;
+	}
+	
+	
+	
+	
+	//TODO de
+	exit(data->last_exit_status);
 }
