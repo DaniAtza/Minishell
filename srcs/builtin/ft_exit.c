1fc5c9a53dfb633fcbf9cd2780b1abceceab3fb9
Dani-42 <d.atienza89@gmail.com>
2025-07-24 22:38:48 +0200

    convert_exit_num

--- srcs/builtin/ft_exit.c
+++ srcs/builtin/ft_exit.c
@@ -12,39 +12,68 @@
 
 #include "minishell.h"
 
-static int	no_num(char *arg)
+static int	is_valid_number(char *arg)
 {
-	int	i;
-
-	i = 0;
-	if(!arg)
-		return(1);
-	while(arg[i])
+	int	i = 0;
+	
+	if (!arg)
 	{
-		if(!ft_isdigit(arg[i]))
-		{
-			ft_putendl_fd("exit: ", STDERR_FILENO);
-			ft_putendl_fd(arg, STDERR_FILENO);
-			ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+		printf("exit\n");
+		return (1);
+	}
+	while (arg[i] && ft_isspace(arg[i]))
+		i++;
+	if (arg[i] == '+' || arg[i] == '-')
+		i++;
+	if (!arg[i])
+		return (0);
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
 			return (0);
-		}
 		i++;
 	}
 	return (1);
 }
 
+static int	convert_exit_num(char *arg)
+{
+	long long	num;
+	int			exit_status;
+
+	num = atoll(arg); //TODO
+	exit_status = (int)(num % 256);
+	if (exit_status < 0)
+		exit_status += 256;
+	return (exit_status);
+}
+
 void	ft_exit(char **argv, t_data *data)
 {
+	int	exit_status;
 
-	if(!no_num(argv[1]))
+
+	if(!is_valid_number(argv[1]))
 	{
+		printf("exit\n");  // if is father
+		ft_putstr_fd("exit: ", STDERR_FILENO);
+		ft_putstr_fd(argv[1], STDERR_FILENO);
+		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
 		data->last_exit_status = 2;
+	}
+	else if(argv[2])
+	{
+		printf("exit\n");  // if is father
+		ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
+		data->last_exit_status = 1;
 		return ;
 	}
-	
-	
-	
-	
-	//TODO de
-	exit(data->last_exit_status);
+	if(argv[1])
+	{
+		printf("exit\n");
+		data->last_exit_status = convert_exit_num(argv[1]);
+	}
+	exit_status = data->last_exit_status;
+	//TODO free
+	exit(exit_status);
 }
