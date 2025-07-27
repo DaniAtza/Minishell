71a0199ff89c150074c360e6443c4ea0122d2b68
D. Atza <d.atienza89@gmail.com>
2025-07-27 19:30:13 +0200

    feat: error_built-in (#44)
    Co-authored-by: Daniel Atienza Romero <datienza@cbr13s5.42barcelona.com>

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: datienza <datienza@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 20:33:18 by datienza          #+#    #+#             */
-/*   Updated: 2025/07/26 19:08:48 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/07/27 18:00:29 by datienza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 # include <stdlib.h>
 # include <string.h>
 
-int		ft_echo(char **argv);
-int		ft_cd(char **argv, t_data *data);
-int		ft_pwd(char *current_pwd);
-int		ft_export(char **argv, t_env *env_list);
-int		ft_unset(char **argv, t_env *env_list);
-void	ft_exit(char **argv, t_pipeline *pipeline, t_data *data);
+void		ft_echo(char **argv, t_data *data);
+int			ft_cd(char **argv, t_data *data);
+void		ft_pwd(char *current_pwd, t_data *data);
+int			ft_export(char **argv, t_env *env_list, t_data *data);
+void		ft_unset(char **argv, t_env *env_list, t_data *data);
+void		ft_exit(char **argv, t_pipeline *pipeline, t_data *data);
 
-int		is_builtin(char **argv);
-int		exe_builtin(char **argv, t_pipeline *pipeline, t_data *data);
+int			is_builtin(char **argv);
+int			exe_builtin(char **argv, t_pipeline *pipeline, t_data *data);
 
 #endif
