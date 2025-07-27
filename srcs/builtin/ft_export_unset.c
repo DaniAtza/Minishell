25f9a35f59056ad01a569ea7cd032d10284d2c75
Daniel Atienza Romero <datienza@cbr13s5.42barcelona.com>
2025-07-27 18:08:15 +0200

    error_built-in

--- srcs/builtin/ft_export_unset.c
+++ srcs/builtin/ft_export_unset.c
@@ -12,20 +12,21 @@
 
 #include "minishell.h"
 
-static void	print_export_error(char *arg)
+static int	print_export_error(char *arg)
 {
 	char	*error1;
 	char	*error2;
 
 	error1 = ft_strjoin("export: `", arg);
 	if (!error1)
-		return ;
+		return (-1);
 	error2 = ft_strjoin(error1, "': not a valid identifier");
 	free(error1);
 	if (!error2)
-		return ;
+		return (-1);
 	ft_putendl_fd(error2, STDERR_FILENO);
 	free(error2);
+	return (-2);
 }
 
 static int	validate_variable_name(char *arg)
@@ -34,33 +35,45 @@ static int	validate_variable_name(char *arg)
 
 	i = 1;
 	if (!ft_isalpha(arg[0]) && arg[0] != '_')
-		return (print_export_error(arg), -1);
+		return (print_export_error(arg));
 	while (arg[i] && arg[i] != '=')
 	{
 		if (!ft_isalnum(arg[i]) && arg[i] != '_')
-			return (print_export_error(arg), -1);
+			return (print_export_error(arg));
 		i++;
 	}
 	return (0);
 }
 
-int	ft_export(char **argv, t_env *env_list)
+int	ft_export(char **argv, t_env *env_list, t_data *data)
 {
 	size_t	i;
+	int		error_num;
+	size_t	flag;
 
+	flag = 0;
 	if (!argv[1])
 		print_env_list_export(env_list);
 	i = 1;
 	while (argv[i])
 	{
-		if (validate_variable_name(argv[i]) == 0)
+		error_num = validate_variable_name(argv[i]);
+		if (error_num == 0)
 			update_env_node(argv[i], &env_list);
+		else if (error_num == -1)
+			return (-1);
+		else if (error_num == -2 && flag == 0)
+			flag = 1;
 		i++;
 	}
+	if (flag == 1)
+		data->last_exit_status = 1;
+	else
+		data->last_exit_status = 0;
 	return (0);
 }
 
-int	ft_unset(char **argv, t_env *env_list)
+void	ft_unset(char **argv, t_env *env_list, t_data *data)
 {
 	int	i;
 
@@ -70,5 +83,5 @@ int	ft_unset(char **argv, t_env *env_list)
 		delete_env_node(argv[i], &env_list);
 		i++;
 	}
-	return (0);
+	data->last_exit_status = 0;
 }
--- srcs/builtin/is_exe_builtin.c
+++ srcs/builtin/is_exe_builtin.c
@@ -6,7 +6,7 @@
 /*   By: datienza <datienza@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 12:01:22 by datienza          #+#    #+#             */
-/*   Updated: 2025/07/08 19:06:16 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/07/27 17:58:23 by datienza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,17 @@ int	exe_builtin(char **argv, t_pipeline *pipeline, t_data *data)
 	if (!argv || !argv[0])
 		return (-1);
 	if (ft_strcmp(argv[0], "echo") == 0)
-		ft_echo(argv);
+		ft_echo(argv, data);
 	else if (ft_strcmp(argv[0], "pwd") == 0)
-		ft_pwd(data->current_pwd);
+		ft_pwd(data->current_pwd, data);
 	else if (ft_strcmp(argv[0], "cd") == 0)
-		ft_cd(argv, data);
+		return (ft_cd(argv, data));
 	else if (ft_strcmp(argv[0], "env") == 0)
-		print_env_list(data->env_list);
+		print_env_list(data->env_list, data);
 	else if (ft_strcmp(argv[0], "export") == 0)
-		ft_export(argv, data->env_list);
+		return (ft_export(argv, data->env_list, data));
 	else if (ft_strcmp(argv[0], "unset") == 0)
-		ft_unset(argv, data->env_list);
+		ft_unset(argv, data->env_list, data);
 	else if (ft_strcmp(argv[0], "exit") == 0)
 		ft_exit(argv, pipeline, data);
 	return (0);
