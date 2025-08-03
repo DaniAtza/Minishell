c4fc9124df415d617fcb86d4cd7661c921810e64
David Agredano <davidagredano@gmail.com>
2025-08-03 09:59:57 +0200

    feat: Add redirection expansions

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -12,6 +12,33 @@
 
 #include "minishell.h"
 
+static void	expand_redirect_words(t_redirect *redirects, t_data *data)
+{
+	t_word		*word;
+	t_redirect	*current;
+
+	current = redirects;
+	while (current)
+	{
+		if (contains_parameters(current->filename)
+			|| contains_quotes(current->filename))
+		{
+			word = create_word(current->filename);
+			identify_segments(word);
+			identify_quoted_segments(word);
+			if (contains_parameters(word->string))
+			{
+				expand_parameters(word, data);
+				//split_word()
+			}
+			remove_quotes(word);
+			concat_segments(word);
+			current->filename = word->string; // TODO: memleak;
+		}
+		current = current->next;
+	}
+}
+
 static void	expand_argv_words(char **argv, t_data *data)
 {
 	t_word	*word;
@@ -46,7 +73,7 @@ int	expand_words(t_process *processes, t_data *data)
 	while (current)
 	{
 		expand_argv_words(current->argv, data);
-		//expand_redirect_words(current->redirects, env_list);
+		expand_redirect_words(current->redirects, data);
 		current = current->next;
 	}
 	return (0);
