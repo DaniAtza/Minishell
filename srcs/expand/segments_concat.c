5ee880963c5cbbf7ae397d9a6be39ad1c5113f6c
David Agredano <davidagredano@gmail.com>
2025-08-05 02:28:59 +0200

    style: segments_concat norm

--- srcs/expand/segments_concat.c
+++ srcs/expand/segments_concat.c
@@ -6,19 +6,28 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 13:49:10 by dagredan          #+#    #+#             */
-/*   Updated: 2025/08/05 01:54:15 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/08/05 02:26:43 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	handle_null_argument(t_word *word, char **new_string)
+{
+	if (word->segments->type == PARAMETER)
+	{
+		free(*new_string);
+		*new_string = NULL;
+	}
+}
+
 static int	is_unquoted_quote_segment(t_segm *segment)
 {
-	return (segment->type == QUOTE 
+	return (segment->type == QUOTE
 		&& !segment->single_quoted && !segment->double_quoted);
 }
 
-size_t	get_total_segments_len(t_word *word)
+static size_t	get_new_string_len(t_word *word)
 {
 	t_segm	*current;
 	size_t	total_len;
@@ -44,14 +53,14 @@ int	concat_word_segments(t_word *word)
 	size_t	total_len;
 	char	*new_string;
 
-	total_len = get_total_segments_len(word);
+	total_len = get_new_string_len(word);
 	new_string = (char *)ft_calloc(total_len + 1, sizeof(char));
 	if (!new_string)
 		return (perror_return("concat_word_segments: malloc", -1));
 	current = word->segments;
 	while (current)
 	{
-		if (is_unquoted_quote_segment(current)) 
+		if (is_unquoted_quote_segment(current))
 		{
 			current = current->next;
 			continue ;
@@ -60,13 +69,7 @@ int	concat_word_segments(t_word *word)
 		current = current->next;
 	}
 	if (ft_strlen(new_string) == 0)
-	{
-		if (word->segments->type == PARAMETER)
-		{
-			free(new_string);
-			new_string = NULL;
-		}
-	}
+		handle_null_argument(word, &new_string);
 	free(word->string);
 	word->string = new_string;
 	return (0);
