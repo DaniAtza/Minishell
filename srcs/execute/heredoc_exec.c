754010560bddf88593f954a9abebe9776d419a20
David Agredano Granell <dagredan@student.42barcelona.com>
2025-08-05 19:57:29 +0200

    refactor: Last cleanup

--- srcs/execute/heredoc_exec.c
+++ srcs/execute/heredoc_exec.c
@@ -19,7 +19,7 @@ static int	write_heredoc(t_redirect *redir, t_data *data)
 
 	fd = open(redir->filename, redir->flags, redir->mode);
 	if (fd == -1)
-		exit(-1);
+		exit(1);
 	while (1)
 	{
 		line = readline("> ");
@@ -28,7 +28,9 @@ static int	write_heredoc(t_redirect *redir, t_data *data)
 		if (ft_strcmp(line, redir->delimiter) == 0)
 			break ;
 		if (redir->expand_heredoc)
-			line = expand_heredoc_line(line, data); // TODO: malloc failure
+			line = expand_heredoc_line(line, data);
+		if (!line)
+			exit(1);
 		ft_putendl_fd(line, fd);
 		free(line);
 	}
