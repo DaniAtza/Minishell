796e653796e1883343693d48f261d9e83d9aeac0
D. Atza <d.atienza89@gmail.com>
2025-08-05 17:22:23 +0200

    feat: Minimal env on initialization (#61)

--- includes/env.h
+++ includes/env.h
@@ -6,12 +6,17 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 15:51:54 by dagredan          #+#    #+#             */
-/*   Updated: 2025/07/26 18:07:21 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/08/05 15:30:34 by datienza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
+
+# define DEF_PATH "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin\
+:/bin"
+# define DEF_SHLVL "SHLVL=1"
+# define DEF_UNDERSCORE "_=/usr/bin/env"
 
 # include "types.h"
 
@@ -37,5 +42,8 @@ void	print_env_list_export(t_env *env_list);
 // Env array
 
 char	**env_list_to_array(t_env *env_list);
+char	**create_minimal_env(void);
+int		ensure_essential_env_vars(t_env **env_list_addr);
+char	*create_pwd_var(void);
 
 #endif
